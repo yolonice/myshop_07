49f16e2bae2f5e63e689032d3c050546cb6e92af
choochoo <243179530@qq.com>
2022-07-27 10:31:11 +0800

    初始化

---
+++ src/components/one.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "demo",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 关闭非生产环境打包的提示
 Vue.config.productionTip = false
 
 new Vue({
